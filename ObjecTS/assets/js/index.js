f555c5a96fa5224670cb040d679b69740cbce5e9
robbykrlos <robby_krlos@yahoo.com>
2016-01-31 17:26:01 +0200

    Service link
    Made link between backend and UI.

--- ObjecTS/assets/js/index.js
+++ ObjecTS/assets/js/index.js
@@ -2,12 +2,28 @@
  * Created by RobbyKrlos on 30.01.2016.
  */
 var objectModule = angular.module('objects-app',[]);
-objectModule.controller('ObjectsCtrl', function(){
+
+objectModule.controller('ObjectsCtrl', function(objRepository){
     var objectCtrl = this;
-    objectCtrl.objects = [{id:'1',type:'2',name:'apple'}, {id:'2',type:'2',name:'peach'}];
+    //objectCtrl.objects = [objRepository.getAllObjects()];
+    objectCtrl.objects = objRepository.getAllObjects();
+    
+    objRepository.getAllObjects().success(function(result) {
+        objectCtrl.objects = {rows: result, cols: Object.keys(result[0])};
+        console.log(objectCtrl.objects.cols);
+        console.log(objectCtrl.objects.rows);
+    });
     
     objectCtrl.getObjectList = function(){
         return objectCtrl.objects;
     };
-    
+});
+
+objectModule.factory('objRepository', function($http) {
+    return {
+        getAllObjects: function() {
+            var url = "http://localhost/objects/lst";
+            return $http.get(url);
+        }
+    };
 });
